5e6af24c5f0f36381f9cf0580582f912532c4418
WernerReyes <werner.reyes@tecsup.edu.pe>
2025-11-18 16:24:33 -0500

    refactor: Eliminar comentarios innecesarios en el método toArray de UserResource

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -6,18 +6,13 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class UserResource extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array<string, mixed>|null
-     */
+    
     public function toArray($request)
     {
         if (empty($this->resource)) {
             return null;
         }
-
+        
         return [
             'id' => $this->id,
             'name' => $this->name,
